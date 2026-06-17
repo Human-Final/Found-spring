165c69f1436bd4fe080b1e1a16b242e62331c873
신민철 <isuss0824@gmail.com>
2026-06-17 14:50:05 +0900

    최종 경찰청 API DB 저장 완료

--- src/main/java/com/human/found/domain/found/service/FoundPoliceServiceImpl.java
+++ src/main/java/com/human/found/domain/found/service/FoundPoliceServiceImpl.java
@@ -25,24 +25,31 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
     @Value("${police.api.url}")
     private String apiUrl;
 
+    // 경찰청 API에서 습득물 데이터를 조회하여 DB에 저장
     @Override
     public String savePoliceFoundItems() {
 
+        // HTTP 요청 객체
         RestTemplate restTemplate = new RestTemplate();
+        // JSON 파싱용 객체
         ObjectMapper objectMapper = new ObjectMapper();
 
+        // 페이지당 만개씩 받음
         int pageNo = 1;
-        int numOfRows = 1000;
+        int numOfRows = 10000;
 
-        int insertCount = 0;
-        int skipCount = 0;
+        int insertCount = 0; // 저장 성공 수
+        int skipCount = 0; // 중복 및 제외 수
 
-        String startYmd = "20260614";
+        // 데이터 가져올 기간 정하기
+        String startYmd = "20260117";
         String endYmd = "20260617";
 
         try {
+            // 데이터 없을 때까지 반복
             while (true) {
 
+                // 경찰청 API 요청 URL 생성
                 String url = apiUrl
                         + "?serviceKey=" + serviceKey
                         + "&pageNo=" + pageNo
@@ -52,22 +59,27 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
 
                 System.out.println("호출 URL = " + url);
 
+                // api 호출
                 String json = restTemplate.getForObject(url, String.class);
 
                 System.out.println("API 응답 = " + json);
 
+                // JSON 문자열을 JsonNode로 변환
                 JsonNode root = objectMapper.readTree(json);
 
+                // body > items 추출
                 JsonNode items = root.path("body").path("items");
 
                 System.out.println("pageNo = " + pageNo);
                 System.out.println("items = " + items);
                 System.out.println("items size = " + items.size());
 
+                // 더이상 데이터 없음 break문 통해 종료
                 if (items.isMissingNode() || items.isNull() || items.isEmpty()) {
                     break;
                 }
 
+                // 단일 객체일 경우 배열 형태로 전환
                 if (items.isObject()) {
                     items = objectMapper.createArrayNode().add(items);
                 }
@@ -76,11 +88,13 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
 
                     String atcId = item.path("atcId").asText(null);
 
+                    // actId 없으면 제외
                     if (atcId == null || atcId.isBlank()) {
                         skipCount++;
                         continue;
                     }
 
+                    // 이미 저장되어 있으면 제외
                     if (foundPoliceMapper.existsByAtcId(atcId) > 0) {
                         skipCount++;
                         continue;
@@ -88,6 +102,7 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
 
                     FoundVO vo = new FoundVO();
 
+                    // 기본 데이터 매핑
                     vo.setAtcId(atcId);
                     vo.setId(null);
 
@@ -100,6 +115,7 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
                     String categoryLabel = getCategoryLabel(originalPrdtClNm);
                     vo.setPrdtClNm(categoryLabel);
 
+                    // 습득일 처리
                     String fdYmdStr = item.path("fdYmd").asText(null);
 
                     if (fdYmdStr != null && !fdYmdStr.isBlank()) {
@@ -115,13 +131,16 @@ public class FoundPoliceServiceImpl implements FoundPoliceService {
                             );
                         }
 
+                        // LocalDateTime 변환
                         LocalDateTime fdYmd = fdDate.atStartOfDay();
                         vo.setFdYmd(fdYmd);
                     }
 
+                    // 기본값
                     vo.setDone(0);
                     vo.setIsDeleted(0);
 
+                    // DB 저장
                     int result = foundPoliceMapper.insertFoundPolice(vo);
 
                     if (result > 0) {
