f0406ba858e0ded844d648db4332ba810e1ef798
김태연 <tyk517@naver.com>
2026-06-17 09:13:06 +0900

    카테고리 대분류만 삽입으로 변경, 카카오api app.properties로 이동

--- src/main/java/com/human/found/domain/lost/controller/LostController.java
+++ src/main/java/com/human/found/domain/lost/controller/LostController.java
@@ -1,48 +1,46 @@
 package com.human.found.domain.lost.controller;
 
+import java.security.Principal;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.human.found.domain.lost.service.LostService;
+import com.human.found.domain.lost.vo.LostVO;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.human.found.domain.lost.service.LostPoliceService;
-import com.human.found.domain.lost.vo.LostPoliceVO;
 
-@Controller
-@RequestMapping("/lost")
+
+@Tag(name = "분실물 관리 테스트 API", description = "VS Code에서 확인하는 스웨거")
+@RestController
+@AllArgsConstructor
 public class LostController {
-
-    @Autowired
-    private LostPoliceService lostService;
-
-    @Autowired
-    private com.human.found.infrastructure.map.KakaoMapConfig kakaoMapConfig;
-
-    // 1. 목록 조회 화면
-    @GetMapping("/list")
-    public String list(Model model) {
-        List<LostPoliceVO> goodsList = lostService.getLostGoodsFromDB();
-        model.addAttribute("goodsList", goodsList);
-        return "lost/list";
-    }
-
-    // 2. 데이터 새로고침
-    @GetMapping("/refresh")
-    public String refresh() {
-        lostService.fetchAndSaveLostGoods(1, 10);
-        return "redirect:/lost/list";
+    private final LostService lostService;
+    @PostMapping("/api/lost")
+    public String LostRegister(@ModelAttribute LostVO lostVO, Principal principal) {
+        
+        if(principal != null){
+            lostVO.setId(principal.getName());
+        }else{
+            lostVO.setId("test_member");
+        }
+        
+        lostService.LostRegister(lostVO);
+        
+        return "성공";
     }
     
-    // 3. 상세페이지 (매핑 경로를 /lost/detail로 고쳤습니다)
-    @GetMapping("/detail")
-    public String detail(@RequestParam("atcId") String atcId, Model model) {
-        LostPoliceVO goods = lostService.getDetailByAtcId(atcId);
-        model.addAttribute("goods", goods);
-        model.addAttribute("kakaoJsKey", kakaoMapConfig.getJsKey()); // 키값 전달
-        return "lost/detail"; // 경로 확인하십시오
+    @GetMapping("/api/lost")
+    public List<LostVO> Lostlist() {
+        List<LostVO>lostlist=lostService.getLostlist();
+        return lostlist;
     }
+    
+    
 }
---
+++ src/main/java/com/human/found/domain/lost/controller/LostPoliceController.java
@@ -0,0 +1,48 @@
+package com.human.found.domain.lost.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.human.found.domain.lost.service.LostPoliceService;
+import com.human.found.domain.lost.vo.LostPoliceVO;
+
+@Controller
+@RequestMapping("/lost")
+public class LostPoliceController {
+
+    @Autowired
+    private LostPoliceService lostService;
+
+    @Autowired
+    private com.human.found.infrastructure.map.KakaoMapConfig kakaoMapConfig;
+
+    // 1. 목록 조회 화면
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<LostPoliceVO> goodsList = lostService.getLostGoodsFromDB();
+        model.addAttribute("goodsList", goodsList);
+        return "lost/list";
+    }
+
+    // 2. 데이터 새로고침
+    @GetMapping("/refresh")
+    public String refresh() {
+        lostService.fetchAndSaveLostGoods(1, 1000);
+        return "redirect:/lost/list";
+    }
+    
+    // 3. 상세페이지 (매핑 경로를 /lost/detail로 고쳤습니다)
+    @GetMapping("/detail")
+    public String detail(@RequestParam("atcId") String atcId, Model model) {
+        LostPoliceVO goods = lostService.getDetailByAtcId(atcId);
+        model.addAttribute("goods", goods);
+        model.addAttribute("kakaoJsKey", kakaoMapConfig.getJsKey()); // 키값 전달
+        return "lost/detail"; // 경로 확인하십시오
+    }
+}
