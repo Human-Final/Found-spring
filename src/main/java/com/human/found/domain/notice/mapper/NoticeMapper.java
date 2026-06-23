33e65a7262f3fa035d3e5dbdd55c00788ff2d161
김태연 <tyk517@naver.com>
2026-06-23 11:59:53 +0900

    feature: 공지사항 페이징 구현

--- src/main/java/com/human/found/domain/notice/mapper/NoticeMapper.java
+++ src/main/java/com/human/found/domain/notice/mapper/NoticeMapper.java
@@ -11,7 +11,7 @@ import com.human.found.global.common.paging.PagingVO;
 @Mapper
 public interface NoticeMapper {
     List<NoticeVO> selectNoticeList(PagingVO pagingVO);       // 목록 조회 (중요공지 상단 고정)
-    int selectNoticeCount(PagingVO pagingVO);   // 전체 페이지 알아보기
+    int selectNoticeCount(PagingVO pagingVO);   // 공지사항 총 갯수로 페이지 수 만들어내기
     List<NoticeVO> selectPopupNotices();  // 팝업 공지 리스트 조회
     NoticeVO selectNoticeDetail(Long num);    // 상세보기, 수정할 때 해당 내용 갖고오기
     void updateViewCount(Long num);          // 조회수 증가
